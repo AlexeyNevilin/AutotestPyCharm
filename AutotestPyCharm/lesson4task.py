fa94e267b7e18cca37d284f45e06014f1c893234
lesha <nevalexey@yandex.ru>
2021-12-06 11:36:30 +0500

    new

--- AutotestPyCharm/lesson4task.py
+++ AutotestPyCharm/lesson4task.py
@@ -49,3 +49,47 @@ def ticket(): #ticket()
 #Опишите класс Name.
 #Экземпляр класса создается одной строкой,
 #состоящей из 2-3 слов (на это должна быть проверка).
+class Name:
+    """Клас ФИО"""
+    def __init__(self, fio):
+        """Принимает ФИО одной строкой, 2-3 слова"""
+        if 2 <= len(fio.split()) <= 3:
+            self.fio = fio
+            self.fio = self.fio.split()
+        else:
+            print('Введите ФИО состоящее из 2-3 слов')
+
+    def brief_name(self): #Пример ввода: my_name.brief_name()
+        """Возвращается Фамилию и имя без отчества"""
+        print(self.fio[0] + ' ' + self.fio[1])
+
+    def initials(self): #Пример ввода: my_name.initials()
+        """Возвращает Фамилию и инициалы"""
+        f_ini = ''
+        for i in range(1, len(self.fio)):
+            ini = self.fio[i]
+            f_ini += (ini[0].upper() + '.')
+        f_ini = self.fio[0].title() + ' ' + f_ini
+        print(f_ini)
+
+    @staticmethod
+    def strfname(format): #Пример ввода: Name.strfname('%и. %о. %Ф')
+        """Преобразует по переданному формату format строку"""
+        my_fio = 'Неволин Алексей Павлович'
+        surname, name, patronymic = my_fio.split(' ')
+        str_format = [
+            ('%И', name.capitalize()),
+            ('%и.', name[0].upper() + '.'),
+            ('%Ф', surname.capitalize()),
+            ('%ф.', surname[0].upper() + '.'),
+            ('%О', patronymic.capitalize()),
+            ('%о.', patronymic[0].upper() + '.')
+        ]
+        for old_format, new_format in str_format:
+            format = format.replace(old_format, new_format)
+        print(format)
+
+
+my_name = Name('Неволин Алексей Павлович')
+my_name2 = Name('Неволин Алексей')
+my_name3 = Name('Неволин Алексей Павлович Неволин')
--- AutotestPyCharm/task23.py
+++ AutotestPyCharm/task23.py
@@ -8,11 +8,11 @@ class Name:
         else:
             print('Введите ФИО состоящее из 2-3 слов')
 
-    def brief_name(self):
+    def brief_name(self): #Пример ввода: my_name.brief_name()
         """Возвращается Фамилию и имя без отчества"""
         print(self.fio[0] + ' ' + self.fio[1])
 
-    def initials(self):
+    def initials(self): #Пример ввода: my_name.initials()
         """Возвращает Фамилию и инициалы"""
         f_ini = ''
         for i in range(1, len(self.fio)):
@@ -22,10 +22,23 @@ class Name:
         print(f_ini)
 
     @staticmethod
-    def strfname(format):
+    def strfname(format): #Пример ввода: Name.strfname('%и. %о. %Ф')
         """Преобразует по переданному формату format строку"""
-
+        my_fio = 'Неволин Алексей Павлович'
+        surname, name, patronymic = my_fio.split(' ')
+        str_format = [
+            ('%И', name.capitalize()),
+            ('%и.', name[0].upper() + '.'),
+            ('%Ф', surname.capitalize()),
+            ('%ф.', surname[0].upper() + '.'),
+            ('%О', patronymic.capitalize()),
+            ('%о.', patronymic[0].upper() + '.')
+        ]
+        for old_format, new_format in str_format:
+            format = format.replace(old_format, new_format)
+        print(format)
 
 
 my_name = Name('Неволин Алексей Павлович')
 my_name2 = Name('Неволин Алексей')
+my_name3 = Name('Неволин Алексей Павлович Неволин')
