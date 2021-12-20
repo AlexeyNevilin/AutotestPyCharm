d2935c8e1bef45efd926d944e35b08d18e5abf7a
lesha <nevalexey@yandex.ru>
2021-12-20 17:38:12 +0500

    new

--- AutotestPyCharm/lesson5tasl.py
+++ AutotestPyCharm/lesson5tasl.py
@@ -1,34 +1,29 @@
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import os
+import requests
 
 d = wd.Chrome()
-d.set_window_size(1920,1080)
+d.maximize_window()
 d.get('https://sbis.ru')
 d.find_element(By.LINK_TEXT, 'Поддержка').click()
 scrollTo_block = d.find_element(By.CLASS_NAME, 'sbis_ru-Support-block2__section-mob')
 d.execute_script('return arguments[0].scrollIntoView(true);', scrollTo_block)
 d.find_element(By.CSS_SELECTOR, '[href="/download"]').click()
 
-def read_file(filelinks):
-    with open(filelinks) as input_file:
-        text = input_file.read()
-    return text
+response = requests.get(d.current_url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-def parse_download_link(filelinks):
-    results = []
-    text = read_file(filelinks)
+results = []
+link_list = soup.find('div', {'data-for': "ereport25"})
+load_blocks = link_list.find_all('div', {'class': 'sbis_ru-DownloadNew-loadLink'})
+for link in load_blocks:
+    download_links = link.find('a').get('href')
+    #download_links_name = link.find('a').text
+    #results.append(download_links_name + ': ' + download_links)
+    results.append(download_links)
+    results.append('\n')
 
-    soup = BeautifulSoup(text)
-    link_list = soup.find('div', {'data-for': "ereport25"})
-    loadLinks = link_list.find_all('div', {'class': "sbis_ru-DownloadNew-flex__child"})
-
-    for link in loadLinks:
-        download_links = link.find('div', {'class': "sbis_ru-DownloadNew-loadLink"}).find('a').get('href')
-        results.append({'download_link': download_links})
-        with open('links.txt', 'w') as links:
-            for key, val in results:
-                links.write('{}:{}\n'.format(key, val))
-                links.close()
-    return
+file = open('links.txt', 'w')
+file.writelines(results)
+file.close()
