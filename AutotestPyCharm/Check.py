91cf0dd7dc2cd700125e4e5f39f1f8ef197dce47
lesha <nevalexey@yandex.ru>
2021-12-22 13:35:32 +0500

    new

--- AutotestPyCharm/Check.py
+++ AutotestPyCharm/Check.py
@@ -1,10 +1,42 @@
-class Name:
-    """Клас ФИО"""
-    def __init__(self, fio):
-        """Принимает ФИО одной строкой, 2-3 слова"""
-        self.fio = fio
-        self.fio = self.fio.split()
-        if len(fio.split()) < 2 or len(fio.split()) > 3:
-            raise ValueError('ФИО должно состоять из 2-3 слов')
+import os
+
+'''path = 'C:\\Users\\lesha\\AppData\\Local\\Programs\\Python\\Python39'
+volume_dirs = 0
+volume_file = 0
+volume_file_py = 0
+volume_file_exe = 0'''
 
 
+def volune_catalog():
+    volume_dirs = 0
+    volume_file = 0
+    volume_file_py = 0
+    volume_file_exe = 0
+
+    # Ищем по дереву каталога все папки и файлы
+    for root, dirs, files in os.walk('C:\\Users\\lesha\\AppData\\Local\\Programs\\Python\\Python39'):
+
+        # Считает количество всех папок и файлов
+        volume_dirs += 1
+        volume_file += len(files)
+
+        # Считаем количество всех файлов с расширение .py и .exe
+        for file in files:
+            expansion = os.path.splitext(file)[1]
+
+            if expansion == '.py':
+                volume_file_py += 1
+
+            if expansion == '.exe':
+                volume_file_exe += 1
+
+    # Объединяем для записи в файл
+    results = (f'Количество папок: {volume_dirs} \n'
+               f'Количество файлов: {volume_file}\n'
+               f'Количество файлов с расширением ".py": {volume_file_py}\n'
+               f'Количество файлов с расширением ".exe": {volume_file_exe}')
+
+    # Записываем ссылки в файл
+    file_catalog = open('vol.txt', mode='w')
+    file_catalog.write(results)
+    file_catalog.close()
