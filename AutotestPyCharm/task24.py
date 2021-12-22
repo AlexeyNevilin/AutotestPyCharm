ea057d9b00f7c8d17d9785e91e9f528bb953b826
lesha <nevalexey@yandex.ru>
2021-12-22 11:51:35 +0500

    new

---
+++ AutotestPyCharm/task24.py
@@ -0,0 +1,36 @@
+import os
+path = 'C:\\Users\\lesha\\AppData\\Local\\Programs\\Python\\Python39'
+volume_dirs = 0
+volume_file = 0
+volume_file_py = 0
+volume_file_exe = 0
+
+#Ищем по дереву каталога все папки и файлы
+for root, dirs, files in os.walk(path):
+
+    #Считает количество всех папок и файлов
+    volume_dirs += 1
+    volume_file += len(files)
+
+    #Считаем количество всех файлов с расширение .py
+    for file in files:
+        expansion = os.path.splitext(file)[1]
+        if expansion == '.py':
+            volume_file_py += 1
+
+    # Считаем количество всех файлов с расширением .exe
+    for file in files:
+        expansion = os.path.splitext(file)[1]
+        if expansion == '.exe':
+            volume_file_exe += 1
+
+#Объединяем для записи в файл
+results = (f'Количество папок: {volume_dirs} \n'
+           f'Количество файлов: {volume_file}\n'
+           f'Количество файлов с расширением ".py": {volume_file_py}\n'
+           f'Количество файлов с расширением ".exe": {volume_file_exe}')
+
+#Записываем ссылки в файл
+file = open('volume.txt', mode='w')
+file.write(results)
+file.close()
